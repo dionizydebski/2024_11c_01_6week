9e40226db61d046ee68580e6379889b2c1b54c5c
fz3002 <franc3002@gmail.com>
2024-10-12 14:49:09 +0200

    Add double jump and freeze player rotation

--- Assets/Scripts/Player/BasicPlayerMovement.cs
+++ Assets/Scripts/Player/BasicPlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine.Serialization;
 namespace Player
 {
     //TODO: Fix clipping to the ground while falling and pressing direction key
-    //TODO: Fix character rotating after falling a bit from platform then going direction and clipping ground corner
+    //TODO: Test double jump force with level ready
 
     [RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
     public class BasicPlayerMovement : MonoBehaviour
@@ -16,12 +16,14 @@ namespace Player
         [SerializeField] private float jumpForce;
         private bool _isGrounded;
         private bool _performJump;
+        private int _jumpCount;
         private SpriteRenderer _spriteRenderer;
         private Animator _animator;
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _rigidbody.freezeRotation = true;
             _spriteRenderer = GetComponent<SpriteRenderer>();
             _animator = GetComponent<Animator>();
         }
@@ -30,7 +32,7 @@ namespace Player
         {
             _xInput = Input.GetAxisRaw("Horizontal");
 
-            if (Input.GetButtonDown("Jump") && _isGrounded)
+            if (Input.GetButtonDown("Jump") && _jumpCount > 0)
             {
                 _performJump = true;
             }
@@ -44,6 +46,7 @@ namespace Player
             {
                 _performJump = false;
                 _rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                _jumpCount -= 1;
             }
 
             FlipSprite();
@@ -63,13 +66,12 @@ namespace Player
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            _isGrounded = true;
+            _jumpCount = 2;
             _animator.SetBool("IsGrounded", _isGrounded);
         }
 
         private void OnCollisionExit2D(Collision2D collision)
         {
-            _isGrounded = false;
             _animator.SetBool("IsGrounded", _isGrounded);
         }
     }
