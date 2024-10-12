82e31a296b7929806d3c537e938112e8a8622492
fz3002 <franc3002@gmail.com>
2024-10-12 17:55:49 +0200

    Basic wall jump functionality (To be revised)

--- Assets/Scripts/Player/BasicPlayerMovement.cs
+++ Assets/Scripts/Player/BasicPlayerMovement.cs
@@ -4,8 +4,7 @@ using UnityEngine.Serialization;
 
 namespace Player
 {
-    //TODO: Fix clipping to the ground while falling and pressing direction key
-    //TODO: Test double jump force with level ready
+    //TODO: Change wall slide / wall jump
 
     [RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
     public class BasicPlayerMovement : MonoBehaviour
@@ -14,11 +13,13 @@ namespace Player
         private float _xInput;
         [SerializeField] private float speed;
         [SerializeField] private float jumpForce;
-        private bool _isGrounded;
         private bool _performJump;
         private int _jumpCount;
         private SpriteRenderer _spriteRenderer;
         private Animator _animator;
+        private BoxCollider2D _boxCollider;
+        private float _wallJumpCooldown;
+        private float _mainGravityScale;
 
         private void Awake()
         {
@@ -26,30 +27,71 @@ namespace Player
             _rigidbody.freezeRotation = true;
             _spriteRenderer = GetComponent<SpriteRenderer>();
             _animator = GetComponent<Animator>();
+            _boxCollider = GetComponent<BoxCollider2D>();
+            _mainGravityScale = _rigidbody.gravityScale;
         }
 
         private void Update()
         {
+            print(_jumpCount);
             _xInput = Input.GetAxisRaw("Horizontal");
 
-            if (Input.GetButtonDown("Jump") && _jumpCount > 0)
+            //Double Jump
+            if (_wallJumpCooldown > 0.2f)
             {
-                _performJump = true;
+                if (Input.GetButtonDown("Jump") && _jumpCount > 0)
+                {
+                    _performJump = true;
+                }
+
+                if (OnWall() && !IsGrounded())
+                {
+                    _rigidbody.gravityScale = 0;
+                    _rigidbody.velocity = Vector2.zero;
+                }
+                else
+                {
+                    _rigidbody.gravityScale = _mainGravityScale;
+                }
+            }
+            else
+            {
+                _wallJumpCooldown += Time.deltaTime;
             }
         }
 
         private void FixedUpdate()
         {
-            _rigidbody.velocity = new Vector2(_xInput * speed, _rigidbody.velocity.y);
 
+            if (IsGrounded())
+            {
+                _jumpCount = 2;
+            }
+            else if (OnWall())
+            {
+                _jumpCount = 1;
+            }
+
+            Move();
+
+            Jump();
+
+            FlipSprite();
+        }
+
+        private void Jump()
+        {
             if (_performJump)
             {
                 _performJump = false;
                 _rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
                 _jumpCount -= 1;
             }
+        }
 
-            FlipSprite();
+        private void Move()
+        {
+            _rigidbody.velocity = new Vector2(_xInput*speed, _rigidbody.velocity.y);
         }
 
         private void FlipSprite()
@@ -64,15 +106,15 @@ namespace Player
             }
         }
 
-        private void OnCollisionEnter2D(Collision2D collision)
+        private bool IsGrounded()
         {
-            _jumpCount = 2;
-            _animator.SetBool("IsGrounded", _isGrounded);
+            RaycastHit2D reycastHit = Physics2D.BoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size,0, Vector2.down, 0.1f, LayerMask.GetMask("Ground"));
+            return reycastHit.collider != null;
         }
-
-        private void OnCollisionExit2D(Collision2D collision)
+        private bool OnWall()
         {
-            _animator.SetBool("IsGrounded", _isGrounded);
+            RaycastHit2D reycastHit = Physics2D.BoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size,0, new Vector2(transform.localScale.x, 0), 0.1f, LayerMask.GetMask("Wall"));
+            return reycastHit.collider != null;
         }
     }
 }
