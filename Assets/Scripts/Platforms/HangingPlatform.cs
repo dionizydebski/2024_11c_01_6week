91778f52a0600fc2cd6a5c7f59a2db2c2c0c521f
s28058 <s28058@pjwstk.edu.pl>
2024-10-14 19:56:59 +0200

    Add Hanging Platform

---
+++ Assets/Scripts/Platforms/HangingPlatform.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HangingPlatform : MonoBehaviour
+{
+    [SerializeField] private float breakForce = 10f; 
+    [SerializeField] private float downwardVelocityThreshold = -2f; 
+    [SerializeField] private Rigidbody2D rb;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
+
+            if (playerRb != null && playerRb.velocity.y < downwardVelocityThreshold)
+            {
+                if (collision.relativeVelocity.magnitude > breakForce)
+                {
+                    rb.bodyType = RigidbodyType2D.Dynamic;
+                }
+            }
+        }
+    }
+}
+
