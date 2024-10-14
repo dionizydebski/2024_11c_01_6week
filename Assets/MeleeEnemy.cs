c175a0e5345aae2895ecc7ea8ddcd03d29fdb018
S26647 <s26647@pjwstk.edu.pl>
2024-10-14 20:00:22 +0200

    Finished meleeEnemy

--- Assets/MeleeEnemy.cs
+++ Assets/MeleeEnemy.cs
@@ -11,6 +11,7 @@ public class MeleeEnemy : MonoBehaviour
     [SerializeField] private BoxCollider2D _boxCollider2D;
     [SerializeField] private LayerMask _playerLayer;
     private float _cooldownTimer = Mathf.Infinity;
+    private Health playerHealth;
 
     // Update is called once per frame
     void Update()
@@ -21,15 +22,20 @@ public class MeleeEnemy : MonoBehaviour
         {
             if (_cooldownTimer >= _attackCooldown)
             {
-            
+                _cooldownTimer = 0;
             }
         }
     }
 
     private bool PlayerInSight()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(_boxCollider2D.bounds.center + transform.right * _range, _boxCollider2D.bounds.size, 0, Vector2.left,
+        RaycastHit2D hit = Physics2D.BoxCast(_boxCollider2D.bounds.center + transform.right * _range * transform.localScale.x, _boxCollider2D.bounds.size, 0, Vector2.left,
             0, _playerLayer);
+
+        if (hit.collider != null)
+        {
+            playerHealth = hit.transform.GetComponent<Health>();
+        }
         
         return hit.collider != null;
     }
@@ -37,6 +43,14 @@ public class MeleeEnemy : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(_boxCollider2D.bounds.center + transform.right * _range, _boxCollider2D.bounds.size);
+        Gizmos.DrawWireCube(_boxCollider2D.bounds.center + transform.right * _range * transform.localScale.x, _boxCollider2D.bounds.size);
+    }
+
+    private void DamagePlayer()
+    {
+        if (PlayerInSight())
+        {
+            playerHealth.Damage(_damage);
+        }
     }
 }
