e7ab98330f952e50deec30d2ad0538b5c95e6918
s28058 <s28058@pjwstk.edu.pl>
2024-10-14 19:41:23 +0200

    Add Breakable Platform

---
+++ Assets/Scripts/Platforms/BreakablePlatform.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakablePlatform : MonoBehaviour
+{
+    [SerializeField] private float breakForce = 10f; 
+    [SerializeField] private float downwardVelocityThreshold = -2f; 
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody playerRb = collision.gameObject.GetComponent<Rigidbody>();
+            if (playerRb != null && playerRb.velocity.y < downwardVelocityThreshold)
+            {
+                if (collision.relativeVelocity.magnitude > breakForce)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
+
