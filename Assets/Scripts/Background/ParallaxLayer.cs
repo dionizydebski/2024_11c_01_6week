716f17c44cd37401e2b07dfa61f497d1ca11e935
Dionizy Dębski <133604599+dionizydebski@users.noreply.github.com>
2024-10-17 15:12:27 +0200

    Stared Adding Background
    +/- Bacground
    + Camera control

---
+++ Assets/Scripts/Background/ParallaxLayer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Background
+{
+    [ExecuteInEditMode]
+    public class ParallaxLayer : MonoBehaviour
+    {
+        public float parallaxFactor;
+
+        public void Move(float delta)
+        {
+            Vector3 newPos = transform.localPosition;
+            newPos.x -= delta * parallaxFactor;
+
+            transform.localPosition = newPos;
+        }
+
+    }
+}
