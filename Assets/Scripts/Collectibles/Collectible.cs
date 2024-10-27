6dc3a0416729f8b2c89bfa78bada33087410c84e
s28058 <s28058@pjwstk.edu.pl>
2024-10-27 02:58:20 +0100

    Add abstract Collectible base class

---
+++ Assets/Scripts/Collectibles/Collectible.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Collectibles
+{
+    [RequireComponent(typeof(Collider2D))]
+    public abstract class Collectible : MonoBehaviour
+    {
+        protected abstract void Collect();
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Collect();
+            }
+        }
+    }
+}
