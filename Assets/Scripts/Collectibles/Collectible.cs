7585e0ffb481d08002b10dfc5a9a4775b7d6e19d
s28058 <s28058@pjwstk.edu.pl>
2024-10-28 22:06:29 +0100

    Add pick up sound

--- Assets/Scripts/Collectibles/Collectible.cs
+++ Assets/Scripts/Collectibles/Collectible.cs
@@ -5,12 +5,15 @@ namespace Collectibles
     [RequireComponent(typeof(Collider2D))]
     public abstract class Collectible : MonoBehaviour
     {
+        public AudioClip pickUpSound;
+        
         protected abstract void Collect();
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Player"))
             {
+                AudioSource.PlayClipAtPoint(pickUpSound, transform.position);
                 Collect();
             }
         }
