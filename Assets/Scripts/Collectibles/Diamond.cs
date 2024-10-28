27df9b9daf525ef5a3e398c72f5643746ddb9efb
s28058 <s28058@pjwstk.edu.pl>
2024-10-28 19:39:13 +0100

    Add diamond pick up anim

--- Assets/Scripts/Collectibles/Diamond.cs
+++ Assets/Scripts/Collectibles/Diamond.cs
@@ -5,14 +5,30 @@ namespace Collectibles
 {
     public class Diamond : Collectible
     {
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
         protected override void Collect()
         {
             PlayerInventory playerInventory = FindObjectOfType<PlayerInventory>();
             if (playerInventory != null)
             {
                 playerInventory.AddDiamond();
+                _animator.SetBool("isPickedUp", true);
+                Invoke(nameof(DestroyDiamond), 0.5f);
             }
-            Destroy(gameObject);
+            else
+            {
+                Debug.LogError("PlayerInventory not found! Coin collection failed.");
+            }
         }
+        
+        private void DestroyDiamond()
+        {
+            Destroy(gameObject);
+        } 
     }
 }
