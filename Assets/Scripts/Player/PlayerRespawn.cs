cfddb74fc7d46157ec86a7dba5c6ed6e9cee5e04
s28058 <s28058@pjwstk.edu.pl>
2024-11-11 13:10:00 +0100

    Fix

--- Assets/Scripts/Player/PlayerRespawn.cs
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -4,35 +4,38 @@ namespace Player
 {
     public class PlayerRespawn : MonoBehaviour
     {
-        [SerializeField] private AudioClip checkpointSound;
-       private Transform _currentCheckpoint;
-       private PlayerHealth _playerHealth;
+        private Transform _currentCheckpoint;
+        private PlayerHealth _playerHealth;
 
-       private void Awake()
-       {
+        private AudioManager _audioManager;
+
+        private void Awake()
+        {
            _playerHealth = GetComponent<PlayerHealth>();
-       }
+           _audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+        }
 
-       private void CheckpointRespawn()
-       {
+        private void CheckpointRespawn()
+        {
             transform.position = _currentCheckpoint.position;
             _playerHealth.Respawn();
 
             //TODO: Move camera to the checkpoint
-       }
+        }
 
-       private void OnTriggerEnter2D(Collider2D other)
-       {
-           if (other.CompareTag("Checkpoint"))
-           {
-               _currentCheckpoint = other.transform;
-               other.GetComponent<Collider2D>().enabled = false;
-               Debug.Log("Checkpoint enabled");
-               SoundManager.instance.PlaySound(checkpointSound);
-               other.gameObject.GetComponent<Animator>().SetTrigger("Rise");
-               //TODO: Add animation or smth for checkpoint unlock
-           }
-       }
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Checkpoint"))
+            {
+                _currentCheckpoint = other.transform;
+                other.GetComponent<Collider2D>().enabled = false;
+                Debug.Log("Checkpoint enabled");
+                
+                _audioManager.PlaySFX(_audioManager.checkpoint);
+                other.gameObject.GetComponent<Animator>().SetTrigger("Rise");
+                //TODO: Add animation or smth for checkpoint unlock
+            }
+        }
 
     }
 }
