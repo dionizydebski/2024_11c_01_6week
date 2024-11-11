754cea622cdb8a1edda3cf5df5ecbcb03304b874
fz3002 <franc3002@gmail.com>
2024-11-11 22:53:14 +0100

    Fix popups showing after loading from main menu

--- Assets/Scripts/CloseWindow.cs
+++ Assets/Scripts/CloseWindow.cs
@@ -1,4 +1,5 @@
 using System;
+using LevelMenager;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -6,8 +7,16 @@ public class CloseWindow : MonoBehaviour
 {
     [SerializeField] private float time = 1f;
     private bool _closed = false;
+
+    private void Awake()
+    {
+        EventSystem.OnLoadGame += LoadGame;
+        EventSystem.OnSaveGame += SaveGame;
+    }
+
     private void Update()
     {
+        //Debug.Log(_closed);
         if (!_closed)
         {
             if (time > 0) time -= Time.deltaTime;
@@ -22,5 +31,21 @@ public class CloseWindow : MonoBehaviour
     public void Close()
     {
         gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        EventSystem.OnLoadGame -= LoadGame;
+        EventSystem.OnSaveGame -= SaveGame;
+    }
+
+    private void LoadGame(SaveData data)
+    {
+        _closed = true;
+    }
+
+    private void SaveGame(SaveData data)
+    {
+        _closed = true;
     }
 }
