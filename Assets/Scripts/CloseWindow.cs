b3735ff13a4d33cc38c4e24870f83e2bd0359b94
fz3002 <franc3002@gmail.com>
2024-11-11 23:21:28 +0100

    Fix popups

--- Assets/Scripts/CloseWindow.cs
+++ Assets/Scripts/CloseWindow.cs
@@ -8,12 +8,6 @@ public class CloseWindow : MonoBehaviour
     [SerializeField] private float time = 1f;
     private bool _closed = false;
 
-    private void Awake()
-    {
-        EventSystem.OnLoadGame += LoadGame;
-        EventSystem.OnSaveGame += SaveGame;
-    }
-
     private void Update()
     {
         //Debug.Log(_closed);
@@ -31,21 +25,5 @@ public class CloseWindow : MonoBehaviour
     public void Close()
     {
         gameObject.transform.GetChild(0).gameObject.SetActive(false);
-    }
-
-    private void OnDestroy()
-    {
-        EventSystem.OnLoadGame -= LoadGame;
-        EventSystem.OnSaveGame -= SaveGame;
-    }
-
-    private void LoadGame(SaveData data)
-    {
-        _closed = true;
-    }
-
-    private void SaveGame(SaveData data)
-    {
-        _closed = true;
     }
 }
